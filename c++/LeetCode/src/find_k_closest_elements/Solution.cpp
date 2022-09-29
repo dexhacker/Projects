165784a17c080dfcdc011a40a787d9c63cd2d90a
Farid Abdugalyev <dexhacker@mail.ru>
2022-09-29 11:55:36 +0600

    Update

--- c++/LeetCode/src/find_k_closest_elements/Solution.cpp
+++ c++/LeetCode/src/find_k_closest_elements/Solution.cpp
@@ -5,56 +5,29 @@
 #include "Solution.h"
 #include <cassert>
 
-int find_k_closest_elements::Solution::closestIndex(vector<int> &arr, int x, int start, int finish) {
-    int middle = (start + finish) / 2;
-    if (arr[middle] == x) return middle;
-    if (arr[middle] > x) {
-        if (middle == 0) return middle;
-
-        if (arr[middle - 1] < x and x < arr[middle]) {
-            if (abs(x - arr[middle]) > abs(x - arr[middle - 1])) {
-                return middle - 1;
-            } else {
-                return middle;
-            }
-        }
-
-        return closestIndex(arr, x, 0, middle - 1);
-    } else {
-        if (middle == arr.size() - 1) return middle;
-
-
-        if (arr[middle] < x and x < arr[middle + 1]) {
-            if (abs(x - arr[middle]) > abs(x - arr[middle + 1])) {
-                return middle + 1;
-            } else {
-                return middle;
-            }
-        }
-
-        return closestIndex(arr, x, middle + 1, finish);
-    }
-}
-
 vector<int> find_k_closest_elements::Solution::findClosestElements(vector<int> &arr, int k, int x) {
-    vector<int> answer;
-    int index = closestIndex(arr, x, 0, arr.size() - 1);
-    int l = index - 1;
-    int r = index + 1;
-    answer.push_back(arr[index]);
-
-    --k;
-    while (k > 0) {
-        if ((l < 0) or ((r < arr.size()) and (abs(x - arr[l]) > abs(x - arr[r])))) {
-            answer.push_back(arr[r]);
-            ++r;
-        } else {
-            answer.push_back(arr[l]);
-            --l;
-        }
-        --k;
+    vector<int> dp(arr.size() - k + 1, -1);
+    int sum = 0;
+    for (int i = 0; i < k; i++) {
+        sum += abs(arr[i] - x);
     }
-    sort(answer.begin(), answer.end());
+
+    dp[0] = sum;
+    for (int i = k; i < arr.size(); i++) {
+        sum = sum - abs(arr[i - k] - x) + abs(arr[i] - x);
+        dp[i - k + 1] = sum;
+    }
+
+    int min_sum_index = 0;
+    for (int i = 1; i < dp.size(); i++) {
+        if (dp[i] < dp[min_sum_index]) min_sum_index = i;
+    }
+
+    vector<int> answer;
+    for (int i = 0; i < k && i + min_sum_index < arr.size(); i++) {
+        answer.push_back(arr[min_sum_index + i]);
+    }
+
     return answer;
 }
 
